84b6e61fcb3aec3626212f1ed388ea1c053b63f8
conewaffle <kevinjm.liang@gmail.com>
2019-03-24 19:41:22 +1100

    fixed variable names in weekdetail for clarity

--- app/src/main/java/com/example/infs3634planner/WeekDetail.java
+++ app/src/main/java/com/example/infs3634planner/WeekDetail.java
@@ -31,6 +31,11 @@ public class WeekDetail {
     public int getWeek(){return week;}
     public void setDate(LocalDate date){this.date = date;}
     public LocalDate getDate(){return date;}
+    public String getLab(){return lab;}
+    public void setLab(String lab){this.lab = lab;}
+    public void setLec(String lec){this.lec=lec;}
+    public String getLec(){return lec;}
+    public String getWeekNo(){return weekNo;}
     public String getDateString(){
         DateTimeFormatter md = DateTimeFormatter.ofPattern("dd MMM");
         String s = this.date.format(md);
@@ -42,48 +47,42 @@ public class WeekDetail {
         return s;
     }
 
-    public String getLab(){return lab;}
-    public void setLab(String lab){this.lab = lab;}
-    public void setLec(String lec){this.lec=lec;}
-    public String getLec(){return lec;}
-    public String getWeekNo(){return weekNo;}
-
 
     public static ArrayList<WeekDetail> getWeekDetail(){
         ArrayList<WeekDetail> weekDetails = new ArrayList<>();
         DateTimeFormatter mdy = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
-        int weekNo = 1;
-        String termWeek = "N/A";
+        int week = 1;
+        String weekNo = "N/A";
         String lab = "N/A";
         String lec = "N/A";
-        LocalDate dateNo = LocalDate.parse("31-12-2018",mdy);
+        LocalDate date = LocalDate.parse("31-12-2018",mdy);
 
         for(int i=1;i<54;i++){
 
-            if(weekNo==8||weekNo==23||weekNo==38) { termWeek = "1"; lec = "Introduction & Android Fundamentals"; lab = "Android Studio, Git & App Basics";}
-            else if(weekNo==9||weekNo==24||weekNo==39) { termWeek = "2"; lec = "Activities, Lifecycle & Intents"; lab = "Activities & Intents";}
-            else if(weekNo==10||weekNo==25||weekNo==40) {termWeek = "3"; lec = "Layouts & UI"; lab = "Designing a UI";}
-            else if(weekNo==11||weekNo==26||weekNo==41) {termWeek = "4"; lec = "Lists & Adapters"; lab = "Displaying Items in a List";}
-            else if(weekNo==12||weekNo==27||weekNo==42) {termWeek = "5"; lec = "Fragments & Multi-layout Apps"; lab = "Fragments for Multi-Layout Apps";}
-            else if(weekNo==13||weekNo==28||weekNo==43) {termWeek = "6"; lec = "APIs, Permissions & Libraries"; lab = "APIs & JSON";}
-            else if(weekNo==14||weekNo==29||weekNo==44) {termWeek = "7"; lec = "Networking"; lab = "Networking";}
-            else if(weekNo==15||weekNo==30||weekNo==45) {termWeek = "8"; lec = "Threads & Async Tasks"; lab = "Async Tasks";}
-            else if(weekNo==16||weekNo==31||weekNo==46) {termWeek = "9"; lec = "Data Saving"; lab = "Database";}
-            else if(weekNo==17||weekNo==32||weekNo==47) {termWeek = "10"; lec = "Outlook & Course Summary"; lab = "Revision";}
-            else {termWeek = "Break"; lec = "N/A"; lab = "N/A";}
+            if(week==8||week==23||week==38) { weekNo = "1"; lec = "Introduction & Android Fundamentals"; lab = "Android Studio, Git & App Basics";}
+            else if(week==9||week==24||week==39) { weekNo = "2"; lec = "Activities, Lifecycle & Intents"; lab = "Activities & Intents";}
+            else if(week==10||week==25||week==40) {weekNo = "3"; lec = "Layouts & UI"; lab = "Designing a UI";}
+            else if(week==11||week==26||week==41) {weekNo = "4"; lec = "Lists & Adapters"; lab = "Displaying Items in a List";}
+            else if(week==12||week==27||week==42) {weekNo = "5"; lec = "Fragments & Multi-layout Apps"; lab = "Fragments for Multi-Layout Apps";}
+            else if(week==13||week==28||week==43) {weekNo = "6"; lec = "APIs, Permissions & Libraries"; lab = "APIs & JSON";}
+            else if(week==14||week==29||week==44) {weekNo = "7"; lec = "Networking"; lab = "Networking";}
+            else if(week==15||week==30||week==45) {weekNo = "8"; lec = "Threads & Async Tasks"; lab = "Async Tasks";}
+            else if(week==16||week==31||week==46) {weekNo = "9"; lec = "Data Saving"; lab = "Database";}
+            else if(week==17||week==32||week==47) {weekNo = "10"; lec = "Outlook & Course Summary"; lab = "Revision";}
+            else {weekNo = "Break"; lab="N/A"; lec="N/A";}
 
-            if(weekNo==18){termWeek = "Study";}
-            if(weekNo==33||weekNo==48||weekNo==19||weekNo==20||weekNo==34||weekNo==35){termWeek = "Exams";}
-            if(weekNo==49||weekNo==50){termWeek = "Exams";}
+            if(week==18){weekNo = "Study";}
+            if(week==33||week==48||week==19||week==20||week==34||week==35){weekNo = "Exams";}
+            if(week==49||week==50){weekNo = "Exams";}
 
-            if(weekNo>7&&weekNo<18){termWeek = "T1 Week " + termWeek;}
-            if(weekNo>22&&weekNo<33){termWeek = "T2 Week " + termWeek;}
-            if(weekNo>37&&weekNo<48){termWeek = "T3 Week " + termWeek;}
+            if(week>7&&week<18){weekNo = "T1 Week " + weekNo;}
+            if(week>22&&week<33){weekNo = "T2 Week " + weekNo;}
+            if(week>37&&week<48){weekNo = "T3 Week " + weekNo;}
 
-            weekDetails.add(new WeekDetail(weekNo, termWeek, dateNo, lab, lec));
-            weekNo = weekNo + 1;
-            dateNo = dateNo.plusWeeks(1);
+            weekDetails.add(new WeekDetail(week, weekNo, date, lab, lec));
+            week = week + 1;
+            date = date.plusWeeks(1);
         }
 
         return weekDetails;
